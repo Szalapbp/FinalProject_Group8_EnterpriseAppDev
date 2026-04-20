2808d37a59fab9b6760d4982ef8a3f417fb7dbbc
Drew Spampinato <spampiap@mail.uc.edu>
2026-04-20 12:32:40 -0400

    Updated Controllers

--- src/main/java/com/bearcat/store_api/controllers/HomeController.java
+++ src/main/java/com/bearcat/store_api/controllers/HomeController.java
@@ -1,1 +1,28 @@
 package com.bearcat.store_api.controllers;
+
+
+import com.bearcat.store_api.services.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class HomeController {
+
+    private final ProductService productService;
+
+    @GetMapping("/")
+    public String redirectToHome() {
+        return "redirect:/home";
+    }
+
+    @GetMapping("/home")
+    public String home(Model model) {
+        model.addAttribute("featuredProducts", productService.getFeaturedProducts());
+        return "index";
+    }
+
+
+}
