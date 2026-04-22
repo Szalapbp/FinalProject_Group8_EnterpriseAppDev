9889a371d70a4a72871b7790ac475ffb132b7658
Rahim <zowangrm@mail.uc.edu>
2026-04-22 16:32:52 -0400

    Modified the UI and added security config

--- src/main/java/com/bearcat/store_api/config/StoreApiUserDetailsService.java
+++ src/main/java/com/bearcat/store_api/config/StoreApiUserDetailsService.java
@@ -1,5 +1,33 @@
 package com.bearcat.store_api.config;
 
+import com.bearcat.store_api.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class StoreApiUserDetailsService implements UserDetailsService {
+
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        com.bearcat.store_api.entities.User user = userRepository.findByEmail(username).orElseThrow(() -> new
+                UsernameNotFoundException("User details not found for the user: " + username));
+
+        return new User(user.getEmail(), user.getPasswordHash(), List.of());
+    }
+
+
+}
+
 
 
 
